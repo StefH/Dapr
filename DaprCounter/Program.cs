0af78ee93c0fa9a6c77ed5cf9a9f2a516b33cb84
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-05-06 21:57:51 +0200

    Add comments

--- DaprCounter/Program.cs
+++ DaprCounter/Program.cs
@@ -9,8 +9,8 @@ namespace DaprCounter;
 class Program
 {
     // static string StoreNameSecret = "envvar-secret-store";
-    static string StoreNameSecret = "local-secret-store";
-    static string StoreName = "statestore-azure-storage-account";
+    static string StoreNameSecret = "local-secret-store"; // The location from the local json file is defined in "secretstores.local.file.yaml"
+    static string StoreName = "statestore-azure-storage-account"; // This StoreName is defined in "azureblob.yaml"
 
     static async Task Main(string[] args)
     {
@@ -18,15 +18,18 @@ class Program
 
         var daprClient = new DaprClientBuilder().Build();
 
+        // 1. Get the DaprAzureStorageAccountKey from the local secrets (as an example)
         var accountKey = await daprClient.GetSecretAsync(StoreNameSecret, "DaprAzureStorageAccountKey");
         Console.WriteLine($"DaprAzureStorageAccountKey = '{accountKey.Values.First()}'");
 
+        // 2. Get the current state from the counter for that StoreName (see "azureblob.yaml" the auth --> secretStore property)
         var counter = await daprClient.GetStateAsync<int>(StoreName, key);
 
         while (true)
         {
             Console.WriteLine($"Counter = {counter++}");
 
+            // 3. Update the value to ++
             await daprClient.SaveStateAsync(StoreName, key, counter);
             await Task.Delay(2000);
         }
