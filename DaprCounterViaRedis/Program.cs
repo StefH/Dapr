de2c45975512eabb95db70f7a0b809ea901bb5dd
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-05-06 22:31:23 +0200

    state.redis

---
+++ DaprCounterViaRedis/Program.cs
@@ -0,0 +1,31 @@
+using Dapr.Client;
+
+namespace DaprCounterViaRedis;
+
+/// <summary>
+/// Run:
+/// - dapr run --app-id DaprCounterViaRedis --components-path ./components dotnet run
+/// </summary>
+class Program
+{
+    static string StoreName = "statestore"; // This StoreName is the default Redis store
+
+    static async Task Main(string[] args)
+    {
+        const string key = "counter";
+
+        var daprClient = new DaprClientBuilder().Build();
+
+        // 1. Get the current state from the counter for that Redis StoreName
+        var counter = await daprClient.GetStateAsync<int>(StoreName, key);
+
+        while (true)
+        {
+            Console.WriteLine($"Counter = {counter++}");
+
+            // 2. Update the value to ++
+            await daprClient.SaveStateAsync(StoreName, key, counter);
+            await Task.Delay(2000);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Dapr
-Some Dapr examples
+
+Default component configuration files are stored in the `$HOME/.dapr/components` folder on
+Linux/macOS, and in the `%USERPROFILE%\.dapr\components` folder on Windows.
+
+## Dapr List
+```
+PS C:\Users\StefHeyenrath> dapr list
+  APP ID       HTTP PORT  GRPC PORT  APP PORT  COMMAND     AGE  CREATED              PID
+  DaprCounter  55777      55778      0         dotnet run  41s  2022-05-06 22:10.23  16332
+```
